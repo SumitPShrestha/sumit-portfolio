62b8568768bd3f55a6598d21d4ccb5a44f019147
Sumit Prakash Shrestha <sshrestha@advalent.com>
2024-03-28 19:45:09 +0545

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroSectionComponent } from './common/components/hero-section/hero-section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: HeroSectionComponent, path: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './common/components/top-nav-bar/top-nav-bar.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { HeroSectionComponent } from './common/components/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    FooterComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
